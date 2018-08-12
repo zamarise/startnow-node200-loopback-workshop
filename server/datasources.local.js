e1ce1d29bbc3925920462b1674eb17c760909171
Zamarise Montoya <zamarise@gmail.com>
2018-08-11 19:54:33 -0700

    Initial commit

---
+++ server/datasources.local.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  db: {
+    name: 'db',
+    connector: 'memory',
+  },
+  MongoDB: {
+    host: '',
+    port: 0,
+    url: '',
+    database: '',
+    password: '',
+    name: 'MongoDB',
+    user: '',
+    connector: 'mongodb',
+  },
+};
