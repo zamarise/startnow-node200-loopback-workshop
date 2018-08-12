04dee179bc761198d6476ed1869554c5d939ba14
Zamarise Montoya <zamarise@gmail.com>
2018-08-11 21:23:18 -0700

    Update server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var loopback = require('loopback');
+require('morgan');
 var boot = require('loopback-boot');
 
-var app = module.exports = loopback();
+var app = (module.exports = loopback());
 
 app.start = function() {
   // start the web server
@@ -24,6 +25,5 @@ boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) app.start();
 });
